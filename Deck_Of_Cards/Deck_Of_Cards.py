fd170086107e38a4063b0784e2b5480c747fbb2d
Shashank Shekhar <shashankshekhar1997@outlook.com>
2017-07-17 07:52:26 +0530

    Added '.' in final output message.

--- Deck_Of_Cards/Deck_Of_Cards.py
+++ Deck_Of_Cards/Deck_Of_Cards.py
@@ -10,7 +10,7 @@ deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
 random.shuffle(deck)
 
 if ans == 'Y' or ans == 'y':
-    print("\nYou got: ",deck[1][0], "of" , deck[1][1] , "\n")
+    print("\nYou got: ",deck[1][0], "of" , deck[1][1] , ".\n")
     exit()
 else:
     print("\nBad input, closing program.\n")
